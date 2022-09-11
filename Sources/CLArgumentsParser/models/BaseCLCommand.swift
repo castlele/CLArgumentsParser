86ef49d6dc9b27f7027cdb048892f06bb7eb87aa
Nikita Semenov <castlelecs@gmail.com>
2022-09-11 12:40:09 +0300

    fix: internal init

--- Sources/CLArgumentsParser/models/BaseCLCommand.swift
+++ Sources/CLArgumentsParser/models/BaseCLCommand.swift
@@ -3,4 +3,11 @@ public struct BaseCLCommand<Option: OptionType>: CommandProtocol {
     public var type: CommandType
     public var arguments: [String]
     public var options: [Option]
+
+    public init(name: String, type: CommandType, arguments: [String] = [], options: [Option] = []) {
+        self.name = name
+        self.type = type
+        self.arguments = arguments
+        self.options = options
+    }
 }
