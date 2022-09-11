c8a1cd7f16b0aeb6467fe31d53731db5ee516b72
Nikita Semenov <castlelecs@gmail.com>
2022-09-11 17:07:04 +0300

    fix: mutating methods on register object

--- Sources/CLArgumentsParser/viewModels/protocols/Registry.swift
+++ Sources/CLArgumentsParser/viewModels/protocols/Registry.swift
@@ -1,21 +1,21 @@
-public protocol RegistryProtocol {
+public protocol RegistryProtocol: AnyObject {
     associatedtype Command: CommandProtocol
 
     var registeredCommands: [String: Command] { get set }
     var registeredOptions: [String: Command.Option] { get set }
 
-    mutating func register(command: Command) 
-    mutating func register(option: Command.Option) 
+    func register(command: Command) 
+    func register(option: Command.Option) 
     func getCommand(_ name: String) -> Command?
     func getOption(_ name: String) -> Command.Option?
 }
 
 public extension RegistryProtocol {
-    mutating func register(command: Command) {
+    func register(command: Command) {
         registeredCommands[command.name] = command
     }
 
-    mutating func register(option: Command.Option) {
+    func register(option: Command.Option) {
         registeredOptions[option.stringValue] = option
     }
 
